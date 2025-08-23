789e55040b29622d39b7d9c1113ed9f4f35000aa
Guraasees Singh <guraaseessingh389@gmail.com>
2025-08-23 14:39:41 +0530

    Frontend Alert of Incoming messages

---
+++ echo-fe/src/App.js
@@ -0,0 +1,27 @@
+import { jsx as _jsx, jsxs as _jsxs, Fragment as _Fragment } from "react/jsx-runtime";
+import { useEffect, useRef, useState } from 'react';
+import './App.css';
+function App() {
+    // const [input, setInput] =  useState<string>("")
+    const [server, setServer] = useState();
+    const inputRef = useRef();
+    function sendMessage() {
+        if (!server) {
+            return;
+        }
+        const msg = inputRef.current?.value;
+        //@ts-expect-error  use generics for now we ignore ts complains 
+        server.send(msg);
+    }
+    //it works one time and connection shouldn't re establish every time we connect 
+    useEffect(() => {
+        const ws = new WebSocket('ws://localhost:8080');
+        setServer(ws);
+        ws.onmessage = function (msg) {
+            alert(msg.data);
+        };
+    }, []);
+    return (_jsx(_Fragment, { children: _jsxs("div", { className: 'flex flex-col h-screen gap -3 bg-black-900 text-white-900 justify-center items-center ', children: [_jsx("h1", { className: "text-5xl p-2 m-2 font-bold", children: "Welcome to Echo Room!" }), _jsx("input", { type: 'text', ref: inputRef, className: 'text-white-900 rounded-sm w-2/4 outline border-r m-4 border-white-900 active:color-white-900 bg-black-900', placeholder: 'Message....' }), _jsx("button", { onClick: sendMessage, className: 'text-black-900 bg-white-900 rounded-sm h-8 w-16 p-1 m-8 outline border-r border-white-900 ', children: "Send Message" })] }) }));
+}
+export default App;
+//# sourceMappingURL=App.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,34 @@
-import { WebSocketServer } from 'ws'
-// const port = 8080
-// const wss = new WebSocketServer({port})
-//or below manner 
-//note port is case sensitive always use 'port'
-const wss = new WebSocketServer({ port: 8080 });
-
-wss.on("connection", (socket) => {
-  console.log("✅ User Connected at ws://localhost:8080");
-
-  socket.send(JSON.stringify({ system: "Welcome to Echo App 👋" }));
-
-  socket.on("message", (data) => {
-    const msg = data.toString();
-    if(msg ==="ping" || msg==="Ping"){
-        socket.send(JSON.stringify({ echo:"pong"}));
-        
-    }else{
-
-        socket.send(JSON.stringify({ echo:msg}));
-    }
-    // Echo back to same client
-
-    // Optional: broadcast to all clients (like group chat)
-    wss.clients.forEach((client) => {
-      if (client.readyState === 1 && client !== socket) {
-        client.send(JSON.stringify({ user: msg }));
+import { WebSocketServer,WebSocket } from "ws";
+//const PORT = 8080
+//const wss = new WebSocketServer({port:PORT})
+//or other way
+const wss = new WebSocketServer({port:8080})
+const allClients :WebSocket[] = [] 
+wss.on("connection",function(socket){
+  console.log("Connection established at: ws://localhost:8080")
+  allClients.push(socket)
+  socket.send(JSON.stringify({system:"Welcome to echo Hall!!!😃"}))
+  socket.on("message",(e)=>{
+    const data = e.toString()
+    // as per kirat 
+    let msg=""
+    if(data==="ping"){
+      msg ="pong pong pong!!!"
+      }else{
+        msg = data
       }
-    });
-  });
-
-  socket.on("close", () => {
-    console.log("❌ User Disconnected");
-  });
-});
-// wss is server  and socket is for evry chat ig 
+   for(let i=0; i<allClients.length; i++){            
+     if(allClients[i] === socket){
+      continue
+     }
+      //@ts-ignore
+      allClients[i].send(JSON.stringify({user:msg}))
+   } 
+    //for every client connection to server there is new socket or client as below we did  client !==socket i didn't understood
+    // wss.clients.forEach((client) => {  // means wss.clients is a array of all active clients on wss server 
+    //   if (client.readyState === 1 && client !== socket) {  //.forEach (client) so we pick every client in array of wss.client 
+    //     client.send(JSON.stringify({ user: msg }));   //clients.readyState ===1 means if the client is active ig i.e connection is eastbalished and client !== socket
+    //   }
+    // })
+  })
+})
